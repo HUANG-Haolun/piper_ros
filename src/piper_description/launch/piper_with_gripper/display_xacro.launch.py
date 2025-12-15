18975f3eba56836d919c5738aea2c757fc6f5d2a
Haolun <mihawkhhuang@gmail.com>
2025-12-15 15:51:42 +0800

    add the dual arm urdf

--- src/piper_description/launch/piper_with_gripper/display_xacro.launch.py
+++ src/piper_description/launch/piper_with_gripper/display_xacro.launch.py
@@ -10,8 +10,9 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
     urdf_tutorial_path = get_package_share_path('piper_description')
-    default_model_path = urdf_tutorial_path / 'urdf/piper_description.xacro'
     default_rviz_config_path = urdf_tutorial_path / 'rviz/piper_ctrl.rviz'
+    # default_model_path = urdf_tutorial_path / 'urdf/piper_description.xacro'
+    default_model_path = urdf_tutorial_path / 'urdf/two_piper_description.urdf'
 
     gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
                                     description='Flag to enable joint_state_publisher_gui')
